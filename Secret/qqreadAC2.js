0ee4d866599f76ffbdc04d090cc107be5f14856d
CenBomin <70682745+chenborming@users.noreply.github.com>
2020-12-14 11:34:53 +0800

    dataupdate

--- Secret/qqreadAC2.js
+++ Secret/qqreadAC2.js
@@ -699,6 +699,7 @@ function showmsg() {
 
 
 
+
 console.log('QQREAD_BODY copy下面的值:'+'\n\n'+qqreadbodyVal+'\n\n'+'QQREAD_TIMEURL copy下面的值'+'\n\n'+qqreadtimeurlVal+'\n\n'+'QQREAD_TIMEHD copy下面的值'+'\n\n'+qqreadtimeheaderVal)
 
 
