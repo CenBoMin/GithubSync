f709c07956cd22d0ea15acb1899ca50a2c48b2cb
CenBomin <70682745+chenborming@users.noreply.github.com>
2020-12-14 11:28:39 +0800

    Update qqread_secrets.js

--- Secret/qqread_secrets.js
+++ Secret/qqread_secrets.js
@@ -700,7 +700,7 @@ function showmsg() {
 
 
 
-#Secrets
+
 
 console.log('QQREAD_BODY copy下面的值:'+'\n\n'+qqreadbodyVal+'\n\n'+'QQREAD_TIMEURL copy下面的值'+'\n\n'+qqreadtimeurlVal+'\n\n'+'QQREAD_TIMEHD copy下面的值'+'\n\n'+qqreadtimeheaderVal)
 
