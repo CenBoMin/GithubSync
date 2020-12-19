e26fdca94c56b465836cf56849729ad8c40ece2d
CenBomin <70682745+chenborming@users.noreply.github.com>
2020-12-19 14:22:18 +0800

    Update Conghua.js

--- CONGHUA/Conghua.js
+++ CONGHUA/Conghua.js
@@ -114,12 +114,12 @@ function signInfo() {
 
 function signInfo() {
     return new Promise((resolve, reject) => {
-        const infourl = {
-            url: 'https://app.kxp.com/user/v1/user/profile',
+        let infourl = {
+            url: `https://app.kxp.com/user/v1/user/profile`,
             headers: JSON.parse(signheaderVal),
         }
         $.post(infourl, (error, response, data) => {
-            signinfo = JSON.parse(data);
+            let signinfo = JSON.parse(data);
             if (signinfo.code == '200') {
                 console.log(`\n【收益总计】${signinfo.data.score}金币 现金约${signinfo.data.money}元\n\n(今天获取${signinfo.data.today_score}金币)\n`);
             }
