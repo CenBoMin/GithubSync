5b73de88e64a15b5804bba1c687c5952464a4c8c
CenBomin <2564885669@qq.com>
2020-12-20 12:41:53 +0800

    调试2

--- CONGHUA/all_function.js
+++ CONGHUA/all_function.js
@@ -2,28 +2,25 @@ let K = 0;
    all()
 function all(){
       signheaderVal = cookiesArr[K];
-   for(var i=0;i<7;i++)
+   for(var i=0;i<4;i++)
  { (function(i) {
             setTimeout(function() {
 
      if (i==0)
 conghuatask();//任务列表
 
-else if (i==1)
-sharevideo();//宝箱
+else if (i==1&& task.data&&task.data.tasklist[0].status==2)
+sharevideo();//金币签到
 
-else if (i==4&& task.data&&task.data.tasklist[6].status==1)
-QQreadsign();//金币签到
-
-else if (i==5&& task.data&&task.data.tasklist[6].status==0)
-QQreadvideo();//视频任务
+else if (i==2&& task.data&&task.data.tasklist[6].status==0)
+everdaycoin();//视频任务
 
 
 
-else if (i == 6 && K < QQreadhdArr.length - 1) {
+else if (i == 3 && K < cookiesArr.length - 1) {
 K += 1;
 all();
- } else if (i == 6 && K == QQreadhdArr.length - 1) {
+} else if (i == 3 && K == cookiesArr.length - 1) {
 	 showmsg();//通知
 	 console.log(tz)
             $.done();
