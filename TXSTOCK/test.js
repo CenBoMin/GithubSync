92ed0e91e26f51b08f7f52a0ccd28bab7a4bbfe1
CenBomin <2564885669@qq.com>
2021-01-10 22:23:58 +0800

    txstock updata

---
+++ TXSTOCK/test.js
@@ -0,0 +1,19 @@
+//将Thu Sep 20 2018 16:23:03 GMT+0800 (中国标准时间)转换为"2018-09-20 16:23:03"
+
+function formatDateTime(inputTime) {
+    var date = new Date(inputTime);
+    var y = date.getFullYear();
+    var m = date.getMonth() + 1;
+    m = m < 10 ? ('0' + m) : m;
+    var d = date.getDate();
+    d = d < 10 ? ('0' + d) : d;
+    var h = date.getHours();
+    h = h < 10 ? ('0' + h) : h;
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+    minute = minute < 10 ? ('0' + minute) : minute;
+    second = second < 10 ? ('0' + second) : second;
+    return y + m + d;
+};
+
+console.log(formatDateTime(new Date()));
