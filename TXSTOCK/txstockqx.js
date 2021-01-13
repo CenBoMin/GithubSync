308af632c81472e943b5354bbe3e71a2cc0e6e04
CenBomin <2564885669@qq.com>
2021-01-13 13:19:27 +0800

    Update txstockqx.js

--- TXSTOCK/txstockqx.js
+++ TXSTOCK/txstockqx.js
@@ -119,7 +119,7 @@ if ($.isNode()) {
     return;
   }
   console.log(`\n✅ 执行【App】日常任务\n`)
-  await task1(statuid2);
+  await task1(statuid2);//测试任务状态
   await task2();
   await task3();
   await task4();
