b4dda193ec03d7fbda02dd9df8715e756d05a54a
CenBomin <2564885669@qq.com>
2021-01-28 10:24:36 +0800

    Update cowopenbox.js

--- TXSTOCK/cowopenbox.js
+++ TXSTOCK/cowopenbox.js
@@ -1,6 +1,6 @@
 const jsname = '腾讯自选股'
 const $ = Env(jsname)
-const logs = 1; //0为关闭日志，1为开启,默认为0
+const logs = 0; //0为关闭日志，1为开启,默认为0
 const notifyInterval = 1; //0为关闭通知，1为所有通知,默认为0
 
 let rndtime = Math.round(new Date().getTime()) //毫秒
