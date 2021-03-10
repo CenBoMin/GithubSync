fab222b1e015b2d2488dc9c07f1c182513ae600c
Cenbomin <2564885669@qq.com>
2021-03-10 16:12:03 +0800

    sync

--- MyTest.js
+++ MyTest.js
@@ -1,4 +1,82 @@
-//转盘判定
-"dailyLotteryLimit" : 10,
-"currentSessionJoinTimes" : 9,
-"totalNum" : 9,
+{
+  "status" : "200",
+  "data" : {
+    "adStrategy" : {
+      "priorityConfig" : [
+        {
+          "maxRequestTimes" : 999999,
+          "hthirdAdCode" : "945769036",
+          "adSource" : 2,
+          "drawType" : 0,
+          "exposureLimit" : 999999,
+          "isBackUp" : false,
+          "cacheExpire" : 1800,
+          "lthirdAdCode" : "945769036",
+          "priority" : 1,
+          "adCodePrice" : 3
+        },
+        {
+          "maxRequestTimes" : 999999,
+          "hthirdAdCode" : "945769039",
+          "adSource" : 2,
+          "drawType" : 0,
+          "exposureLimit" : 999999,
+          "isBackUp" : false,
+          "cacheExpire" : 1800,
+          "lthirdAdCode" : "945769039",
+          "priority" : 2,
+          "adCodePrice" : 2
+        },
+        {
+          "maxRequestTimes" : 999999,
+          "hthirdAdCode" : "3031046717941465",
+          "adSource" : 5,
+          "drawType" : 0,
+          "exposureLimit" : 999999,
+          "isBackUp" : false,
+          "cacheExpire" : 1800,
+          "lthirdAdCode" : "3031046717941465",
+          "priority" : 3,
+          "adCodePrice" : 1
+        }
+      ],
+      "priceRangeConfig" : [
+
+      ]
+    },
+    "extInfo" : {
+      "adNewUser" : true,
+      "prizeUnit" : "100金币",
+      "prizeFrequency" : 1,
+      "prizeInfo" : {
+        "prizeStatus" : 1,
+        "prizeContent" : "{\"prizeValue\":100,\"bizType\":\"205\",\"appId\":\"1\",\"asac\":\"2A208066PP49L7GO1LNV24\",\"isNeedDynamicPrizeValue\":false,\"isManualReceive\":false,\"expireTime\":86400}",
+        "prizeImgUrl" : "http://img-tailor.11222.cn/cms/upload/img/161097845880116069979052860e9fadd8053d28fc6fd9fb396ce76af3.png",
+        "prizeId" : 1047,
+        "prizeType" : 30,
+        "updatedAt" : 1611050659,
+        "createdAt" : 1610978478,
+        "prizeDesc" : "100金币",
+        "prizeName" : "100金币"
+      },
+      "showSubName" : "看小视频得金币",
+      "buttonText" : "看小视频得金币",
+      "dayCount" : 0,
+      "chanceCurrentCnt" : 10,
+      "prizeId" : 1047,
+      "chanceMaxCnt" : 10,
+      "actorNum" : 746343441
+    },
+    "resourceId" : 719,
+    "dataTracks" : "{\"ad_request_id\":\"2008148079_719_1615354471157_36\",\"blackGroupId\":\"\",\"groupId\":\"\",\"resourceId\":\"6\"}",
+    "freeAdTime" : 0,
+    "adSource" : 6,
+    "endTime" : 1675223999,
+    "deliveryId" : 897,
+    "materialType" : 0,
+    "thirdAdCode" : "",
+    "startTime" : 1609430400,
+    "adPlanType" : 2
+  },
+  "message" : "成功"
+}
