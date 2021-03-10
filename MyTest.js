4037bb578cbbbcd1e3c4f07260321e3ad14a93fa
Cenbomin <2564885669@qq.com>
2021-03-10 16:43:54 +0800

    sync

--- MyTest.js
+++ MyTest.js
@@ -1,82 +1,128 @@
 {
   "status" : "200",
   "data" : {
-    "adStrategy" : {
-      "priorityConfig" : [
+    "actEndTime" : 1617206399,
+    "singleLotteryMaxPrizesNum" : 1,
+    "isStart" : true,
+    "actDesc" : "幸运大转盘抽惊喜礼品",
+    "actInfo" : {
+      "goldcoinLotteryFreeAvailableNum" : 0,
+      "dailyLotteryLimit" : 10,
+      "availableNum" : 10,
+      "isSupportGoldcoin" : 1,
+      "goldcoinLotteryPrice" : 100,
+      "isSupportDouLottery" : 0,
+      "currentSessionJoinTimes" : 10,
+      "totalNum" : 20,
+      "currentSessionId" : 1,
+      "douLotteryPrice" : 0,
+      "totalPeople" : 3972,
+      "douLotteryAvailableNum" : 0,
+      "sessions" : [
         {
-          "maxRequestTimes" : 999999,
-          "hthirdAdCode" : "945769036",
-          "adSource" : 2,
-          "drawType" : 0,
-          "exposureLimit" : 999999,
-          "isBackUp" : false,
-          "cacheExpire" : 1800,
-          "lthirdAdCode" : "945769036",
-          "priority" : 1,
-          "adCodePrice" : 3
-        },
-        {
-          "maxRequestTimes" : 999999,
-          "hthirdAdCode" : "945769039",
-          "adSource" : 2,
-          "drawType" : 0,
-          "exposureLimit" : 999999,
-          "isBackUp" : false,
-          "cacheExpire" : 1800,
-          "lthirdAdCode" : "945769039",
-          "priority" : 2,
-          "adCodePrice" : 2
-        },
-        {
-          "maxRequestTimes" : 999999,
-          "hthirdAdCode" : "3031046717941465",
-          "adSource" : 5,
-          "drawType" : 0,
-          "exposureLimit" : 999999,
-          "isBackUp" : false,
-          "cacheExpire" : 1800,
-          "lthirdAdCode" : "3031046717941465",
-          "priority" : 3,
-          "adCodePrice" : 1
+          "beginTime" : 1615305600,
+          "endTime" : 1615391999,
+          "dailyJoinNum" : 10,
+          "sessionId" : 1,
+          "asac" : "2A20C07RJ9F51AOEFSNHDR",
+          "ext" : "",
+          "prizePool" : [
+            {
+              "prizeRecordId" : null,
+              "prizeContent" : "",
+              "prizeImgUrl" : "http://img-tailor.11222.cn/pm/book/operate/2020121011165918.png",
+              "prizeId" : 0,
+              "prizeType" : 0,
+              "createdAt" : null,
+              "prizeName" : "谢谢参与",
+              "prizeDesc" : "",
+              "isBigPrize" : false
+            },
+            {
+              "prizeRecordId" : null,
+              "prizeContent" : "{\"prizeValue\":88,\"bizType\":210,\"appId\":\"1\",\"isNeedIdentifyRisk\":false,\"asac\":null,\"backUpPrizeId\":\"\"}",
+              "prizeImgUrl" : "http://img-tailor.11222.cn/cms/upload/img/1599448905158f4d7668dbec46a9740b13a8623df2cbf.png",
+              "prizeId" : 863,
+              "prizeType" : 30,
+              "createdAt" : null,
+              "prizeName" : "88金币",
+              "prizeDesc" : "金币大转盘奖励",
+              "isBigPrize" : false
+            },
+            {
+              "prizeRecordId" : null,
+              "prizeContent" : "{\"prizeValue\":188,\"bizType\":210,\"appId\":\"1\",\"isNeedIdentifyRisk\":false,\"asac\":null,\"backUpPrizeId\":\"\"}",
+              "prizeImgUrl" : "http://img-tailor.11222.cn/cms/upload/img/1599449101083f4d7668dbec46a9740b13a8623df2cbf.png",
+              "prizeId" : 865,
+              "prizeType" : 30,
+              "createdAt" : null,
+              "prizeName" : "188金币",
+              "prizeDesc" : "金币大转盘奖励",
+              "isBigPrize" : false
+            },
+            {
+              "prizeRecordId" : null,
+              "prizeContent" : "{\"prizeValue\":288,\"bizType\":210,\"appId\":\"1\",\"isNeedIdentifyRisk\":false,\"asac\":null,\"backUpPrizeId\":\"\"}",
+              "prizeImgUrl" : "http://img-tailor.11222.cn/cms/upload/img/1599449170694f4d7668dbec46a9740b13a8623df2cbf.png",
+              "prizeId" : 866,
+              "prizeType" : 30,
+              "createdAt" : null,
+              "prizeName" : "288金币",
+              "prizeDesc" : "金币大转盘奖励",
+              "isBigPrize" : false
+            },
+            {
+              "prizeRecordId" : null,
+              "prizeContent" : "{\"prizeValue\":1000000,\"bizType\":\"210\",\"appId\":\"1\",\"isNeedIdentifyRisk\":true,\"asac\":\"2A208066PP49L7GO1LNV24\",\"backUpPrizeId\":\"806\",\"isNeedDynamicPrizeValue\":false,\"isManualReceive\":false,\"expireTime\":86400,\"isCash\":false}",
+              "prizeImgUrl" : "http://img-tailor.11222.cn/cms/upload/img/1614912919769b961b84f1e6f728d700518287a87f477.png",
+              "prizeId" : 1202,
+              "prizeType" : 30,
+              "createdAt" : null,
+              "prizeName" : "100元红包",
+              "prizeDesc" : "福利转盘抽奖",
+              "isBigPrize" : false
+            },
+            {
+              "prizeRecordId" : null,
+              "prizeContent" : "{\"prizeValue\":500,\"bizType\":210,\"appId\":\"1\",\"isNeedIdentifyRisk\":false,\"asac\":null,\"backUpPrizeId\":\"\"}",
+              "prizeImgUrl" : "http://img-tailor.11222.cn/cms/upload/img/1599449214847733650a2625f45759977fd20d7a9c871.png",
+              "prizeId" : 867,
+              "prizeType" : 30,
+              "createdAt" : null,
+              "prizeName" : "500金币",
+              "prizeDesc" : "金币大转盘奖励",
+              "isBigPrize" : false
+            },
+            {
+              "prizeRecordId" : null,
+              "prizeContent" : "{\"prizeValue\":1,\"bizType\":666,\"appId\":\"1\",\"isNeedIdentifyRisk\":false,\"backUpPrizeId\":\"\",\"isManualReceive\":false,\"expireTime\":86400}",
+              "prizeImgUrl" : "http://img-tailor.11222.cn/cms/upload/img/1607501097561054e09a886f698aeeb5aff0d88d53490.png",
+              "prizeId" : 806,
+              "prizeType" : 30,
+              "createdAt" : null,
+              "prizeName" : "1金币",
+              "prizeDesc" : "金币奖励",
+              "isBigPrize" : false
+            },
+            {
+              "prizeRecordId" : null,
+              "prizeContent" : "",
+              "prizeImgUrl" : "http://img-tailor.11222.cn/cms/upload/img/16096691895691607570907075d19f9c4f014e921c2213383cdf06aa39.png",
+              "prizeId" : 1022,
+              "prizeType" : 11,
+              "createdAt" : null,
+              "prizeName" : "华为手机",
+              "prizeDesc" : "华为手机",
+              "isBigPrize" : false
+            }
+          ]
         }
-      ],
-      "priceRangeConfig" : [
-
       ]
     },
-    "extInfo" : {
-      "adNewUser" : true,
-      "prizeUnit" : "100金币",
-      "prizeFrequency" : 1,
-      "prizeInfo" : {
-        "prizeStatus" : 1,
-        "prizeContent" : "{\"prizeValue\":100,\"bizType\":\"205\",\"appId\":\"1\",\"asac\":\"2A208066PP49L7GO1LNV24\",\"isNeedDynamicPrizeValue\":false,\"isManualReceive\":false,\"expireTime\":86400}",
-        "prizeImgUrl" : "http://img-tailor.11222.cn/cms/upload/img/161097845880116069979052860e9fadd8053d28fc6fd9fb396ce76af3.png",
-        "prizeId" : 1047,
-        "prizeType" : 30,
-        "updatedAt" : 1611050659,
-        "createdAt" : 1610978478,
-        "prizeDesc" : "100金币",
-        "prizeName" : "100金币"
-      },
-      "showSubName" : "看小视频得金币",
-      "buttonText" : "看小视频得金币",
-      "dayCount" : 0,
-      "chanceCurrentCnt" : 10,
-      "prizeId" : 1047,
-      "chanceMaxCnt" : 10,
-      "actorNum" : 746343441
-    },
-    "resourceId" : 719,
-    "dataTracks" : "{\"ad_request_id\":\"2008148079_719_1615354471157_36\",\"blackGroupId\":\"\",\"groupId\":\"\",\"resourceId\":\"6\"}",
-    "freeAdTime" : 0,
-    "adSource" : 6,
-    "endTime" : 1675223999,
-    "deliveryId" : 897,
-    "materialType" : 0,
-    "thirdAdCode" : "",
-    "startTime" : 1609430400,
-    "adPlanType" : 2
+    "actTitle" : "幸运大转盘",
+    "timestamp" : 1615354248,
+    "actType" : "LOTTERY",
+    "actStartTime" : 1612281600
   },
   "message" : "成功"
 }
