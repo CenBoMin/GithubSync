0949a8a04c8b363cd6f8218e931fbfbdca8cc631
Cenbomin <2564885669@qq.com>
2021-03-14 11:44:18 +0800

    sync

---
+++ TXSTOCK/readme.js
@@ -0,0 +1,53 @@
+
+╭┉┉╮╭╮╭╮╭┉┉╮╭┉┉╮╭┉┉╮╭┉┉╮╭╮╭╮
+╰╮╭╯┋╰╯┋┋╭┉╯╰╮╭╯┋╭╮┋┋╭┉╯┋╰╯┋
+ ┋┋ ╰╮╭╯┋╰┉╮ ┋┋ ┋┋┋┋┋┋  ┋ ╭╯
+ ┋┋ ╭╯╰╮╰┉╮┋ ┋┋ ┋┋┋┋┋┋  ┋ ╰╮
+ ┋┋ ┋╭╮┋╭┉╯┋ ┋┋ ┋╰╯┋┋╰┉╮┋╭╮┋
+ ╰╯ ╰╯╰╯╰┉┉╯ ╰╯ ╰┉┉╯╰┉┉╯╰╯╰╯
+
+//!!!:
+【BOXJS】：https://raw.githubusercontent.com/CenBoMin/GithubSync/main/cenbomin.box.json
+
+#运行脚本前请先下载app,登录。。。手动完成成长任务,有钱
+#8.8打卡任务也手动做,建议只打卡4个成功即可,第5个基本拿不到
+#微信小程序-腾讯自选股和app的日常任务是分开的,毛比较少,手动做吧！
+#复制以下链接,贴到到微信点击,帮我助力
+https://zqact01.tenpay.com/activity/page/200814appInvite/#/new?stat_data=Ovy34p00pn019&invite_code=i02Fvx18dc&nick_name=%E9%99%88%E6%9F%8F%E9%8A%98&head_url=https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FQ0j4TwGTfTLWqFtqq3JGenAvqEnZhiazoeCicjicAZhEiaCib1LqSCHtC8sPLbG52YiajLZh8VRkY8bWu0uu24AKjgfA%2F132
+
+
+特别感谢烧烤哥,loon用户可以用这个专门的conf：
+https://raw.githubusercontent.com/CenBoMin/GithubSync/main/TXSTOCK/txsloon_cookie.conf
+
+**** 定时任务 ****
+0 16 * * * https://raw.githubusercontent.com/CenBoMin/GithubSync/main/TXSTOCK/txstockqx.js
+**** 食用方法 ****
+到[重写]-[引用],加入重写引用,根据以下提示获取cookie
+**** 重写引用 ****
+IOS APP：https://raw.githubusercontent.com/CenBoMin/GithubSync/main/TXSTOCK/txs_cookie.conf
+微信小程序：https://raw.githubusercontent.com/CenBoMin/GithubSync/main/TXSTOCK/txs_wxcookie.conf
+**** IOS APP ****
+taskheader：打开app-立即获取
+taskkey：点击左上头像-我的福利-点击获取
+
+userheader：点击左上头像-进入即获取
+userkey：点击左上头像-进入即获取
+
+提现cookie(cashheader):禁用获取cookie重写,重新打开app,点击左上头像-进入,等待我的福利下面活动加载成功,启用获取cookie重写,然后再点击第一个猜涨跌活动的页面就能够获取到了。。。
+
+签到cookie(signheader)：禁用获取cookie重写,重新打开app,点击左上头像-进入,等待右上角的金币数加载成功（进入提现页面）,启用获取cookie重写,点击进入提现的页面,会卡住加载几秒...过一会就能够获取到了
+
+signkey：禁用获取cookie重写,重新打开app,点击左上头像-进入,等待我的福利下面活动加载成功,启用获取cookie重写,然后再点击第一个猜涨跌活动的页面就能够获取到了。。。
+**** 微信小程序 ****
+！！！！！微信和app重写有冲突,不能两个同时开
+！！！！！必须先关闭app重写,在加入微信小程序重写https://raw.githubusercontent.com/CenBoMin/GithubSync/main/TXSTOCK/txs_wxcookie.conf,启用之后就可以去微信小程序做任务获取cookie
+
+微信任务cookie(wxtaskkey):进入微信小程序,点击资讯页面或者股票页面获取
+
+！！！！！获取完cookie,务必关闭重写引用
+#脚本一天尽量运行1-3次,本脚本虽然加了防呆机制,但是还是尽量避免运行多次哦...
+#且用且珍惜,更新无限期。
+
+// 手动做部分:
+# 模拟炒股周赛奖励：星期天登录模拟炒股(微信/app)
+# 周一抢牛活动：周一登录抢牛活动页面（app）
