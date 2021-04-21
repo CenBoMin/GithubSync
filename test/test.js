aa8d431622a6fd1d7ff7bb00d040c6cb973558cd
Cenbomin <2564885669@qq.com>
2021-04-22 03:43:13 +0800

    测试

---
+++ test/test.js
@@ -0,0 +1,28 @@
+{
+  "ok": true,
+  "result": [{
+    "update_id": 329841415,
+    "message": {
+      "message_id": 629,
+      "from": {
+        "id": 1285467394,
+        "is_bot": false,
+        "first_name": "\u6735\u62c9",
+        "username": "Cenbomin",
+        "language_code": "zh-hans"
+      },
+      "chat": {
+        "id": -1001328051322,
+        "title": "\u4e0d\u4e8c\u5bb6\ud83c\udfe0",
+        "type": "supergroup"
+      },
+      "date": 1619009541,
+      "text": "/test \u6267\u610feyeueiwieieieieoroeor",
+      "entities": [{
+        "offset": 0,
+        "length": 5,
+        "type": "bot_command"
+      }]
+    }
+  }]
+}
