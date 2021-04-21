711522ed04c10fe61eb76bd39a51ce5bc12f4047
Cenbomin <2564885669@qq.com>
2021-04-22 02:56:00 +0800

    sync

---
+++ TGBOT/test.js
@@ -0,0 +1,86 @@
+{
+  "ok": true,
+  "result": [{
+    "update_id": 329841413,
+    "message": {
+      "message_id": 625,
+      "from": {
+        "id": 1443684190,
+        "is_bot": false,
+        "first_name": "\u4f60",
+        "last_name": "love",
+        "username": "whatloveme"
+      },
+      "chat": {
+        "id": -1001328051322,
+        "title": "\u4e0d\u4e8c\u5bb6\ud83c\udfe0",
+        "type": "supergroup"
+      },
+      "date": 1618962833,
+      "new_chat_participant": {
+        "id": 1443684190,
+        "is_bot": false,
+        "first_name": "\u4f60",
+        "last_name": "love",
+        "username": "whatloveme"
+      },
+      "new_chat_member": {
+        "id": 1443684190,
+        "is_bot": false,
+        "first_name": "\u4f60",
+        "last_name": "love",
+        "username": "whatloveme"
+      },
+      "new_chat_members": [{
+        "id": 1443684190,
+        "is_bot": false,
+        "first_name": "\u4f60",
+        "last_name": "love",
+        "username": "whatloveme"
+      }]
+    }
+  }, {
+    "update_id": 329841414,
+    "message": {
+      "message_id": 627,
+      "from": {
+        "id": 1443684190,
+        "is_bot": false,
+        "first_name": "\u4f60",
+        "last_name": "love",
+        "username": "whatloveme"
+      },
+      "chat": {
+        "id": -1001328051322,
+        "title": "\u4e0d\u4e8c\u5bb6\ud83c\udfe0",
+        "type": "supergroup"
+      },
+      "date": 1618962873,
+      "text": "\u53c8\u6362\u5934\u50cf\u4e86\u5440"
+    }
+  }, {
+    "update_id": 329841415,
+    "message": {
+      "message_id": 629,
+      "from": {
+        "id": 1285467394,
+        "is_bot": false,
+        "first_name": "\u6735\u62c9",
+        "username": "Cenbomin",
+        "language_code": "zh-hans"
+      },
+      "chat": {
+        "id": -1001328051322,
+        "title": "\u4e0d\u4e8c\u5bb6\ud83c\udfe0",
+        "type": "supergroup"
+      },
+      "date": 1619009541,
+      "text": "/test \u6267\u610feyeueiwieieieieoroeor",
+      "entities": [{
+        "offset": 0,
+        "length": 5,
+        "type": "bot_command"
+      }]
+    }
+  }]
+}
