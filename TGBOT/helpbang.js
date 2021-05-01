1755ee366fd68a3a90379bfc899fe499e4fa1e50
Cenbomin <2564885669@qq.com>
2021-05-01 16:07:40 +0800

    更新互利帮验证码

--- TGBOT/helpbang.js
+++ TGBOT/helpbang.js
@@ -1,2 +1,2 @@
-/helpbang QDqdQMDE,/helpbang QDqdQMDE,/helpbang QDqdQMDL,/helpbang QDqdQMDE,/submit_activity_codes helpbang@QDqdQMDA,/submit_activity_codes helpbang@QDqdQMDE,/submit_activity_codes helpbang@UzwRZTNd @MSwWXAJB @mpsQ5aKy @RA2ppbqn" 1roQ3gJX psQ5aKy
-RA2ppbqn
+/helpbang QDqdQMDE,/helpbang QDqdQMDE,/helpbang QDqdQMDL,/helpbang QDqdQMDE,/submit_activity_codes helpbang@QDqdQMDA,/submit_activity_codes helpbang@QDqdQMDE,/submit_activity_codes helpbang@UzwRZTNd,@MSwWXAJB @mpsQ5aKy,@RA2ppbqn" 1roQ3gJX psQ5aKy
+RA2ppbqn,
