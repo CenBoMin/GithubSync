7cbd532ecc89a4a1da4f836c81516a7a211f1b02
Cenbomin <2564885669@qq.com>
2021-05-26 05:34:40 +0800

    sync

--- UCPIG/ucpigck1.js
+++ UCPIG/ucpigck1.js
@@ -87,9 +87,9 @@ async function GetCookie() {
       ucpigapp[calarrno(len, uid)].videotask1 = videoTaskArr;
       $.setdata(JSON.stringify(ucpigapp, null, 2), 'ucpigapp');
       $.log(`获取成功🎉: videoTaskUrl: ${userurl}`)
-      $.msg($.name, "", `UC小猪猪[账号${calarrno(len,uid)+1}] 获取[视频${videoTid-2200}]数据成功！🎉`);
+      $.msg($.name, "", `UC小猪猪[账号${calarrno(len,uid)+1}] 获取第一天[视频${videoTid-2200}]数据成功！🎉`);
     } else {
-      $.msg($.name, "", 'UC小猪猪[视频]数据获取失败⚠️');
+      $.msg($.name, "", 'UC小猪猪第一天[视频]数据获取失败⚠️');
     }
   }
   //领取任务奖励
@@ -126,9 +126,9 @@ async function GetCookie() {
       ucpigapp[calarrno(len, uid)].videoaward1 = videoAwardArr;
       $.setdata(JSON.stringify(ucpigapp, null, 2), 'ucpigapp');
       $.log(`获取成功🎉: videoAwardUrl: ${userurl}`)
-      $.msg($.name, "", `UC小猪猪[账号${calarrno(len,uid)+1}] 获取[奖励${videoTid-2200}]数据成功！🎉`);
+      $.msg($.name, "", `UC小猪猪[账号${calarrno(len,uid)+1}] 获取第一天[奖励${videoTid-2200}]数据成功！🎉`);
     } else {
-      $.msg($.name, "", 'UC小猪猪[奖励]数据获取失败⚠️');
+      $.msg($.name, "", 'UC小猪猪第一天[奖励]数据获取失败⚠️');
     }
   }
 
--- UCPIG/ucpigck2.js
+++ UCPIG/ucpigck2.js
@@ -87,7 +87,7 @@ async function GetCookie() {
       ucpigapp[calarrno(len, uid)].videotask2 = videoTaskArr;
       $.setdata(JSON.stringify(ucpigapp, null, 2), 'ucpigapp');
       $.log(`获取成功🎉: videoTaskUrl: ${userurl}`)
-      $.msg($.name, "", `UC小猪猪[账号${calarrno(len,uid)+1}] 获取[视频${videoTid-2200}]数据成功！🎉`);
+      $.msg($.name, "", `UC小猪猪[账号${calarrno(len,uid)+1}] 获取第二天[视频${videoTid-2200}]数据成功！🎉`);
     } else {
       $.msg($.name, "", 'UC小猪猪[视频]数据获取失败⚠️');
     }
@@ -126,7 +126,7 @@ async function GetCookie() {
       ucpigapp[calarrno(len, uid)].videoaward2 = videoAwardArr;
       $.setdata(JSON.stringify(ucpigapp, null, 2), 'ucpigapp');
       $.log(`获取成功🎉: videoAwardUrl: ${userurl}`)
-      $.msg($.name, "", `UC小猪猪[账号${calarrno(len,uid)+1}] 获取[奖励${videoTid-2200}]数据成功！🎉`);
+      $.msg($.name, "", `UC小猪猪[账号${calarrno(len,uid)+1}] 获取第二天[奖励${videoTid-2200}]数据成功！🎉`);
     } else {
       $.msg($.name, "", 'UC小猪猪[奖励]数据获取失败⚠️');
     }
