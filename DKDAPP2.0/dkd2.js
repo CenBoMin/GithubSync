c134417d8539c25c5a047b9667e93bc9f8d780a0
Cenbomin <2564885669@qq.com>
2021-06-21 00:49:35 +0800

    sync

--- DKDAPP2.0/dkd2.js
+++ DKDAPP2.0/dkd2.js
@@ -1,3 +1,4 @@
+
 const $=new Env("DKDAPP");var Base64={keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(input){var output="";var chr1,chr2,chr3,enc1,enc2,enc3,enc4;var i=0;input=Base64.utf8encode(input);while(i<input.length){chr1=input.charCodeAt(i++);chr2=input.charCodeAt(i++);chr3=input.charCodeAt(i++);enc1=chr1>>2;enc2=((chr1&3)<<4)|(chr2>>4);enc3=((chr2&15)<<2)|(chr3>>6);enc4=chr3&63;if(isNaN(chr2)){enc3=enc4=64;}else if(isNaN(chr3)){enc4=64;}
 output=output+
 this.keyStr.charAt(enc1)+this.keyStr.charAt(enc2)+
